9056382e837f9034ae6a9c2688ea398a5887aa27
Shivansh Pandey <shivanshp987@gmail.com>
2026-06-01 23:12:57 +0530

    feat(ui): add 14 starter components (forms, overlays, layout, display)

---
+++ packages/ui/src/components/separator/separator.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import { Separator as SeparatorPrimitive } from 'radix-ui'
+import * as React from 'react'
+import { cn } from '../../lib/utils'
+
+const Separator = React.forwardRef<
+  React.ComponentRef<typeof SeparatorPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof SeparatorPrimitive.Root>
+>(({ className, orientation = 'horizontal', decorative = true, ...props }, ref) => (
+  <SeparatorPrimitive.Root
+    ref={ref}
+    decorative={decorative}
+    orientation={orientation}
+    className={cn(
+      'shrink-0 bg-border data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px',
+      className,
+    )}
+    {...props}
+  />
+))
+Separator.displayName = SeparatorPrimitive.Root.displayName
+
+export { Separator }
